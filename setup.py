9d9a98d88c9953f70419010fba652a78f281a6ae
Rujul Nayak <55329600+nayakrujul@users.noreply.github.com>
2022-08-23 09:37:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = 'Default classes - read the docs at https://www.github.com/na
 
 setup(
   name = 'default-class',
-  version = '1.0',
+  version = '1.1',
   license='Apache',
   description = 'Default classes',
   author = 'Rujul Nayak',
